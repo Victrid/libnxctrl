7193ab5ba75c0e8c3799c3a9dd7ea4510e695650
Weihao Jiang <weihau.chiang@gmail.com>
2022-09-01 10:58:02 +0800

    Version 0.1.3: Fixed nxbt import error

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
+from pathlib import Path
+
 from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
         name='libnxctrl',
-        version='0.1.1',
+        version='0.1.3',
         url='https://github.com/Victrid/libnxctrl',
         license='GPLv3',
         author='Weihao Jiang',
         author_email='weihau.chiang@gmail.com',
         description='Python Library Emulating Nintendo Switch Controllers',
-        readme='README.md',
-        readme_type='text/markdown',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
         classifiers=[
             "Development Status :: 3 - Alpha",
             "Topic :: Software Development :: Libraries",
@@ -32,13 +37,16 @@ setup(
             'hid~=1.0.5',
             'aioconsole~=0.5.1'
             ],
-        packages=['libnxctrl',
-                  'libnxctrl.nxbt',
-                  'libnxctrl.mart1no_joycontrol.joycontrol',
-                  'libnxctrl.Poohl_joycontrol.joycontrol'],
+        packages=[
+            "libnxctrl",
+            "libnxctrl.nxbt.nxbt",
+            "libnxctrl.nxbt.nxbt.controller",
+            "libnxctrl.mart1no_joycontrol.joycontrol",
+            "libnxctrl.Poohl_joycontrol.joycontrol"
+            ],
         package_dir={
             'libnxctrl':                               'src/libnxctrl',
-            'libnxctrl.nxbt':                          'src/libnxctrl/nxbt',
+            'libnxctrl.nxbt.nxbt':                     'src/libnxctrl/nxbt/nxbt',
             'libnxctrl.mart1no_joycontrol.joycontrol': 'src/libnxctrl/mart1no_joycontrol/joycontrol',
             'libnxctrl.Poohl_joycontrol.joycontrol':   'src/libnxctrl/Poohl_joycontrol/joycontrol'
             },
@@ -53,13 +61,13 @@ setup(
                 '../LICENSE',
                 '../README.md'
                 ],
-            'libnxctrl.nxbt':                          [
-                'nxbt/controller/sdp/switch-controller.xml'
+            'libnxctrl.nxbt.nxbt':                     [
+                'controller/sdp/switch-controller.xml'
                 '../LICENSE',
                 '../README.md'
                 ],
             'libnxctrl.nxbt.nxbt.controller':          [
-
+                'sdp/switch-controller.xml'
                 ]
             },
         )
